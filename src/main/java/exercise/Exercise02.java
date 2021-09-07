9eaeb5c963a9d9cff06402ed508eb124a4d9cadf
magdasobrino <magdalena.sobrino@knights.ucf.edu>
2021-09-07 17:35:32 -0400

    Initial commit

---
+++ src/main/java/exercise/Exercise02.java
@@ -0,0 +1,26 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class Exercise02 {
+        public static void main(String[] args) {
+            String str1;
+            int rko = 0;
+            Scanner keyboard = new Scanner(System.in);
+
+            System.out.print("What is the input string? ");
+            str1 = keyboard.nextLine();
+
+            if(str1.isEmpty()) {
+                System.out.print("Please enter a string: ");
+                str1 = keyboard.nextLine();
+            }
+            rko = str1.length();
+            System.out.print( str1 + " has " + rko + " characters. ");
+            keyboard.close();
+        }
+
+}
+
+
+
